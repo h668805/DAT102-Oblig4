ef4bdbf916a0452ea1be4a2802ebe36227207071
h668805 <668805@stud.hvl.no>
2024-03-19 13:24:15 +0100

    Update Main.java

--- src/no/hvl/dat102/bilskilt/Main.java
+++ src/no/hvl/dat102/bilskilt/Main.java
@@ -4,14 +4,20 @@ public class Main {
 	public static void main(String[] args) {
 		HashTabell hashTabell = new HashTabell();
 		HashTabell2 hashTabell2 = new HashTabell2();
-		String[] tabell = {"EL65431", "TA14374", "ZX87181", "EL47007", "VV50000", "UV14544", "EL32944"};
-		
+		String[] tabell = { "EL65431", "TA14374", "ZX87181", "EL47007", "VV50000", "UV14544", "EL32944" };
+
 		for (String s : tabell) {
 			hashTabell.leggTil(s);
 			hashTabell2.leggTil(s);
 		}
-		
+
+		// Gjennomsnitlig antall kall av equals-metoden når vi søker etter et element i
+		// tabellen vil være ca. 2, fordi den ikke er for full, og de fleste elementer
+		// ligger på første plass den får tildelt av hashCode()
 		System.out.println(hashTabell);
+		
+		// Gjennomsnitlig antall kall av equals-metoden når vi søker etter et element i
+		// tabellen vil være ca. 1, fordi bare én av listene inneholder 2 elementer.
 		System.out.println(hashTabell2);
 	}
 }
