f42798f41fb6df72ccbe6a0257b1ac2c851fde04
h668805 <668805@stud.hvl.no>
2024-03-19 11:48:56 +0100

    L

--- src/no/hvl/dat102/bilskilt/HashTabell.java
+++ src/no/hvl/dat102/bilskilt/HashTabell.java
@@ -1,6 +1,8 @@
 package no.hvl.dat102.bilskilt;
 
-public class HashTabell implements HashTabellADT {
+import java.util.Arrays;
+
+public class HashTabell {
 	private String[] tabell;
 	private int antall;
 	private static final int STANDARD_KAPASITET = 10;
@@ -14,7 +16,6 @@ public class HashTabell implements HashTabellADT {
 		antall = 0;
 	}
 
-	@Override
 	public String leggTil(String s) {
 		int index = s.hashCode() % tabell.length;
 		while(tabell[index] != null) {
@@ -26,38 +27,7 @@ public class HashTabell implements HashTabellADT {
 	}
 
 	@Override
-	public String fjern(String s) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getVerdi(String s) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean inneholder(String s) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean erTom() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public int getAntall() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void toem() {
-		// TODO Auto-generated method stub
-		
+	public String toString() {
+		return Arrays.toString(tabell);
 	}
 }
